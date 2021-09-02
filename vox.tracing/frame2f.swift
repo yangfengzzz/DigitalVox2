9b0bca09e8ca65815a63acf6f6f3fa640ac77e3f
yangfeng <yangfengzzz@hotmail.com>
2021-09-02 22:20:14 +0800

    add frame3f method

--- vox.tracing/frame2f.swift
+++ vox.tracing/frame2f.swift
@@ -9,11 +9,11 @@ import Foundation
 
 // Rigid frames stored as a column-major affine transform matrix.
 public struct frame2f {
-    var x = vec2f(1, 0)
-    var y = vec2f(0, 1)
-    var o = vec2f(0, 0)
+    public var x = vec2f(1, 0)
+    public var y = vec2f(0, 1)
+    public var o = vec2f(0, 0)
 
-    init(_ x: vec2f, _ y: vec2f, _ o: vec2f) {
+    public init(_ x: vec2f, _ y: vec2f, _ o: vec2f) {
         self.x = x
         self.y = y
         self.o = o
@@ -21,10 +21,20 @@ public struct frame2f {
 
     subscript(i: Int) -> vec2f {
         get {
-            fatalError("TODO")
+            switch i {
+            case 0:return x
+            case 1:return y
+            case 2:return o
+            default:fatalError()
+            }
         }
         set {
-            fatalError("TODO")
+            switch i {
+            case 0:return x = newValue
+            case 1:return y = newValue
+            case 2:return o = newValue
+            default:fatalError()
+            }
         }
     }
 }
@@ -34,55 +44,62 @@ let identity2x3f = frame2f([1, 0], [0, 1], [0, 0])
 // Frame properties
 @inlinable
 func rotation(_ a: frame2f) -> mat2f {
-    fatalError("TODO")
+    mat2f(a.x, a.y)
 }
 
 @inlinable
 func translation(_ a: frame2f) -> vec2f {
-    fatalError("TODO")
+    a.o
 }
 
 // Frame construction
 @inlinable
 func make_frame(_ m: mat2f, _ t: vec2f) -> frame2f {
-    fatalError("TODO")
+    frame2f(m.x, m.y, t)
 }
 
 // Conversion between frame and mat
 @inlinable
 func frame_to_mat(_ f: frame2f) -> mat3f {
-    fatalError("TODO")
+    mat3f([f.x.x, f.x.y, 0], [f.y.x, f.y.y, 0], [f.o.x, f.o.y, 1])
 }
 
 @inlinable
-func mat_to_frame(_ ma: mat3f) -> frame2f {
-    fatalError("TODO")
+func mat_to_frame(_ m: mat3f) -> frame2f {
+    frame2f([m.x.x, m.x.y], [m.y.x, m.y.y], [m.z.x, m.z.y])
 }
 
 // Frame comparisons.
 @inlinable
 func ==(_ a: frame2f, _ b: frame2f) -> Bool {
-    fatalError("TODO")
+    a.x == b.x && a.y == b.y && a.o == b.o
 }
 
 @inlinable
 func !=(_ a: frame2f, _ b: frame2f) -> Bool {
-    fatalError("TODO")
+    !(a == b)
 }
 
 // Frame composition, equivalent to affine matrix product.
 @inlinable
 func *(_ a: frame2f, _ b: frame2f) -> frame2f {
-    fatalError("TODO")
+    make_frame(rotation(a) * rotation(b), rotation(a) * b.o + a.o)
 }
 
 @inlinable
 func *=(_ a: inout frame2f, _ b: frame2f) -> frame2f {
-    fatalError("TODO")
+    a = a * b
+    return a
 }
 
 // Frame inverse, equivalent to rigid affine inverse.
 @inlinable
 func inverse(_ a: frame2f, _  non_rigid: Bool = false) -> frame2f {
-    fatalError("TODO")
+    if (non_rigid) {
+        let minv = inverse(rotation(a))
+        return make_frame(minv, -(minv * a.o))
+    } else {
+        let minv = transpose(rotation(a))
+        return make_frame(minv, -(minv * a.o))
+    }
 }
--- vox.tracing/frame3f.swift
+++ vox.tracing/frame3f.swift
@@ -9,12 +9,12 @@ import Foundation
 
 // Rigid frames stored as a column-major affine transform matrix.
 public struct frame3f {
-    var x = vec3f(1, 0, 0)
-    var y = vec3f(0, 1, 0)
-    var z = vec3f(0, 0, 1)
-    var o = vec3f(0, 0, 0)
+    public var x = vec3f(1, 0, 0)
+    public var y = vec3f(0, 1, 0)
+    public var z = vec3f(0, 0, 1)
+    public var o = vec3f(0, 0, 0)
 
-    init(_ x: vec3f, _ y: vec3f, _ z: vec3f, _ o: vec3f) {
+    public init(_ x: vec3f, _ y: vec3f, _ z: vec3f, _ o: vec3f) {
         self.x = x
         self.y = y
         self.z = z
@@ -23,10 +23,22 @@ public struct frame3f {
 
     subscript(i: Int) -> vec3f {
         get {
-            fatalError("TODO")
+            switch i {
+            case 0:return x
+            case 1:return y
+            case 2:return z
+            case 3:return o
+            default:fatalError()
+            }
         }
         set {
-            fatalError("TODO")
+            switch i {
+            case 0:return x = newValue
+            case 1:return y = newValue
+            case 2:return z = newValue
+            case 3:return o = newValue
+            default:fatalError()
+            }
         }
     }
 }
@@ -37,66 +49,89 @@ let identity3x4f = frame3f([1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0])
 // Frame properties
 @inlinable
 func rotation(_ a: frame3f) -> mat3f {
-    fatalError("TODO")
+    mat3f(a.x, a.y, a.z)
 }
 
 @inlinable
 func translation(_ a: frame3f) -> vec3f {
-    fatalError("TODO")
+    a.o
 }
 
 // Frame construction
 @inlinable
 func make_frame(_ m: mat3f, _ t: vec3f) -> frame3f {
-    fatalError("TODO")
+    frame3f(m.x, m.y, m.z, t)
 }
 
 // Conversion between frame and mat
 @inlinable
 func frame_to_mat(_ f: frame3f) -> mat4f {
-    fatalError("TODO")
+    mat4f([f.x.x, f.x.y, f.x.z, 0],
+            [f.y.x, f.y.y, f.y.z, 0],
+            [f.z.x, f.z.y, f.z.z, 0],
+            [f.o.x, f.o.y, f.o.z, 1])
 }
 
 @inlinable
 func mat_to_frame(_ m: mat4f) -> frame3f {
-    fatalError("TODO")
+    frame3f([m.x.x, m.x.y, m.x.z],
+            [m.y.x, m.y.y, m.y.z],
+            [m.z.x, m.z.y, m.z.z],
+            [m.w.x, m.w.y, m.w.z])
 }
 
 // Frame comparisons.
 @inlinable
 func ==(_ a: frame3f, _ b: frame3f) -> Bool {
-    fatalError("TODO")
+    a.x == b.x && a.y == b.y && a.z == b.z && a.o == b.o
 }
 
 @inlinable
 func !=(_ a: frame3f, _  b: frame3f) -> Bool {
-    fatalError("TODO")
+    !(a == b)
 }
 
 // Frame composition, equivalent to affine matrix product.
 @inlinable
 func *(_ a: frame3f, _ b: frame3f) -> frame3f {
-    fatalError("TODO")
+    make_frame(rotation(a) * rotation(b), rotation(a) * b.o + a.o)
 }
 
 @inlinable
 func *=(_ a: inout frame3f, _ b: frame3f) -> frame3f {
-    fatalError("TODO")
+    a = a * b
+    return a
 }
 
 // Frame inverse, equivalent to rigid affine inverse.
 @inlinable
 func inverse(_ a: frame3f, _ non_rigid: Bool = false) -> frame3f {
-    fatalError("TODO")
+    if (non_rigid) {
+        let minv = inverse(rotation(a))
+        return make_frame(minv, -(minv * a.o))
+    } else {
+        let minv = transpose(rotation(a))
+        return make_frame(minv, -(minv * a.o))
+    }
 }
 
 // Frame construction from axis.
 @inlinable
-func frame_fromz(_ o: vec3f, _ v: vec3f) -> frame3f {
-    fatalError("TODO")
+func frame_from_z(_ o: vec3f, _ v: vec3f) -> frame3f {
+    // https://graphics.pixar.com/library/OrthonormalB/paper.pdf
+    let z = normalize(v)
+    let sign = copysignf(1.0, z.z)
+    let a = -1.0 / (sign + z.z)
+    let b = z.x * z.y * a
+    let x = vec3f(1.0 + sign * z.x * z.x * a, sign * b, -sign * z.x)
+    let y = vec3f(b, sign + z.y * z.y * a, -z.y)
+    return frame3f(x, y, z, o)
 }
 
 @inlinable
 func frame_fromzx(_ o: vec3f, _ z_: vec3f, _ x_: vec3f) -> frame3f {
-    fatalError("TODO")
+    let z = normalize(z_)
+    let x = orthonormalize(x_, z)
+    let y = normalize(cross(z, x))
+    return frame3f(x, y, z, o)
 }
