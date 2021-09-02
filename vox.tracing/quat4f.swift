50f39e034c745d05c3436667333b7a3c7bcd72fa
yangfeng <yangfengzzz@hotmail.com>
2021-09-02 22:23:55 +0800

    add quatf method

--- vox.tracing/quat4f.swift
+++ vox.tracing/quat4f.swift
@@ -14,45 +14,46 @@ let identity_quat4f = quat4f(ix: 0, iy: 0, iz: 0, r: 1)
 // Quaterion operations
 @inlinable
 func dot(_ a: quat4f, _  b: quat4f) -> Float {
-    fatalError("TODO")
+    simd_dot(a, b)
 }
 
 @inlinable
 func length(_  a: quat4f) -> Float {
-    fatalError("TODO")
+    simd_length(a)
 }
 
 @inlinable
 func normalize(_  a: quat4f) -> quat4f {
-    fatalError("TODO")
+    simd_normalize(a)
 }
 
 @inlinable
 func conjugate(_ a: quat4f) -> quat4f {
-    fatalError("TODO")
+    a.conjugate
 }
 
 @inlinable
 func inverse(_ a: quat4f) -> quat4f {
-    fatalError("TODO")
+    a.inverse
 }
 
 @inlinable
 func uangle(_ a: quat4f, _ b: quat4f) -> Float {
-    fatalError("TODO")
+    let d = dot(a, b)
+    return d > 1 ? 0 : acos(d < -1 ? -1 : d)
 }
 
 @inlinable
 func lerp(_ a: quat4f, _ b: quat4f, _ t: Float) -> quat4f {
-    fatalError("TODO")
+    a * (1 - t) + b * t
 }
 
 @inlinable
 func nlerp(_ a: quat4f, _ b: quat4f, _ t: Float) -> quat4f {
-    fatalError("TODO")
+    normalize(lerp(a, b, t))
 }
 
 @inlinable
 func slerp(_ a: quat4f, _ b: quat4f, _ t: Float) -> quat4f {
-    fatalError("TODO")
+    simd_slerp(a, b, t)
 }
