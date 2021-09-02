c829711423f151aa0f82675342e322c6284a0322
yangfeng <yangfengzzz@hotmail.com>
2021-09-02 20:22:50 +0800

    add frame apis

--- vox.tracing/frame2f.swift
+++ vox.tracing/frame2f.swift
@@ -31,3 +31,58 @@ public struct frame2f {
 
 let identity2x3f = frame2f([1, 0], [0, 1], [0, 0])
 
+// Frame properties
+@inlinable
+func rotation(_ a: frame2f) -> mat2f {
+    fatalError("TODO")
+}
+
+@inlinable
+func translation(_ a: frame2f) -> vec2f {
+    fatalError("TODO")
+}
+
+// Frame construction
+@inlinable
+func make_frame(_ m: mat2f, _ t: vec2f) -> frame2f {
+    fatalError("TODO")
+}
+
+// Conversion between frame and mat
+@inlinable
+func frame_to_mat(_ f: frame2f) -> mat3f {
+    fatalError("TODO")
+}
+
+@inlinable
+func mat_to_frame(_ ma: mat3f) -> frame2f {
+    fatalError("TODO")
+}
+
+// Frame comparisons.
+@inlinable
+func ==(_ a: frame2f, _ b: frame2f) -> Bool {
+    fatalError("TODO")
+}
+
+@inlinable
+func !=(_ a: frame2f, _ b: frame2f) -> Bool {
+    fatalError("TODO")
+}
+
+// Frame composition, equivalent to affine matrix product.
+@inlinable
+func *(_ a: frame2f, _ b: frame2f) -> frame2f {
+    fatalError("TODO")
+}
+
+@inlinable
+func *=(_ a: inout frame2f, _ b: frame2f) -> frame2f {
+    fatalError("TODO")
+}
+
+// Frame inverse, equivalent to rigid affine inverse.
+@inlinable
+func inverse(_ a: frame2f, _  non_rigid: Bool = false) -> frame2f {
+    fatalError("TODO")
+}
--- vox.tracing/frame3f.swift
+++ vox.tracing/frame3f.swift
@@ -32,3 +32,71 @@ public struct frame3f {
 }
 
 let identity3x4f = frame3f([1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0])
+
+
+// Frame properties
+@inlinable
+func rotation(_ a: frame3f) -> mat3f {
+    fatalError("TODO")
+}
+
+@inlinable
+func translation(_ a: frame3f) -> vec3f {
+    fatalError("TODO")
+}
+
+// Frame construction
+@inlinable
+func make_frame(_ m: mat3f, _ t: vec3f) -> frame3f {
+    fatalError("TODO")
+}
+
+// Conversion between frame and mat
+@inlinable
+func frame_to_mat(_ f: frame3f) -> mat4f {
+    fatalError("TODO")
+}
+
+@inlinable
+func mat_to_frame(_ m: mat4f) -> frame3f {
+    fatalError("TODO")
+}
+
+// Frame comparisons.
+@inlinable
+func ==(_ a: frame3f, _ b: frame3f) -> Bool {
+    fatalError("TODO")
+}
+
+@inlinable
+func !=(_ a: frame3f, _  b: frame3f) -> Bool {
+    fatalError("TODO")
+}
+
+// Frame composition, equivalent to affine matrix product.
+@inlinable
+func *(_ a: frame3f, _ b: frame3f) -> frame3f {
+    fatalError("TODO")
+}
+
+@inlinable
+func *=(_ a: inout frame3f, _ b: frame3f) -> frame3f {
+    fatalError("TODO")
+}
+
+// Frame inverse, equivalent to rigid affine inverse.
+@inlinable
+func inverse(_ a: frame3f, _ non_rigid: Bool = false) -> frame3f {
+    fatalError("TODO")
+}
+
+// Frame construction from axis.
+@inlinable
+func frame_fromz(_ o: vec3f, _ v: vec3f) -> frame3f {
+    fatalError("TODO")
+}
+
+@inlinable
+func frame_fromzx(_ o: vec3f, _ z_: vec3f, _ x_: vec3f) -> frame3f {
+    fatalError("TODO")
+}
