4367026748ed9d50fe443994b72160be9edc5975
yangfeng <yangfengzzz@hotmail.com>
2021-09-05 15:40:39 +0800

    add sampling

--- vox.tracing/sampling.swift
+++ vox.tracing/sampling.swift
@@ -40,7 +40,7 @@ func rand3f() -> vec3f {
 
 // Shuffles a sequence of elements
 @inlinable
-func shuffle<T>(vals: inout [T]) {
+func shuffle<T>(_ vals: inout [T]) {
     // https://en.wikipedia.org/wiki/Fisher–Yates_shuffle
     for i in stride(from: vals.count - 1, to: 0, by: -1) {
         let j = rand1i(i + 1)
@@ -53,151 +53,154 @@ func shuffle<T>(vals: inout [T]) {
 // -----------------------------------------------------------------------------
 // Sample an hemispherical direction with uniform distribution.
 @inlinable
-func sample_hemisphere(ruv: vec2f) -> vec3f {
-    fatalError()
+func sample_hemisphere(_ ruv: vec2f) -> vec3f {
+    let z: Float = ruv.y
+    let r: Float = sqrt(clamp(1.0 - z * z, 0.0, 1.0))
+    let phi = 2 * Float.pi * ruv.x
+    return [r * cos(phi), r * sin(phi), z]
 }
 
 @inlinable
-func sample_hemisphere_pdf(direction: vec3f) -> Float {
+func sample_hemisphere_pdf(_ direction: vec3f) -> Float {
     fatalError()
 }
 
 // Sample an hemispherical direction with uniform distribution.
 @inlinable
-func sample_hemisphere(normal: vec3f, ruv: vec2f) -> vec3f {
+func sample_hemisphere(_ normal: vec3f, _ ruv: vec2f) -> vec3f {
     fatalError()
 }
 
 @inlinable
-func sample_hemisphere_pdf(normal: vec3f, direction: vec3f) -> Float {
+func sample_hemisphere_pdf(_ normal: vec3f, _ direction: vec3f) -> Float {
     fatalError()
 }
 
 // Sample a spherical direction with uniform distribution.
 @inlinable
-func sample_sphere(ruv: vec2f) -> vec3f {
+func sample_sphere(_ ruv: vec2f) -> vec3f {
     fatalError()
 }
 
 @inlinable
-func sample_sphere_pdf(w: vec3f) -> Float {
-    fatalError()
-}
-
-// Sample an hemispherical direction with cosine distribution.
-@inlinable
-func sample_hemisphere_cos(ruv: vec2f) -> vec3f {
-    fatalError()
-}
-
-@inlinable
-func sample_hemisphere_cos_pdf(direction: vec3f) -> Float {
+func sample_sphere_pdf(_ w: vec3f) -> Float {
     fatalError()
 }
 
 // Sample an hemispherical direction with cosine distribution.
 @inlinable
-func sample_hemisphere_cos(normal: vec3f, ruv: vec2f) -> vec3f {
+func sample_hemisphere_cos(_ ruv: vec2f) -> vec3f {
     fatalError()
 }
 
 @inlinable
-func sample_hemisphere_cos_pdf(normal: vec3f, direction: vec3f) -> Float {
+func sample_hemisphere_cos_pdf(_ direction: vec3f) -> Float {
+    fatalError()
+}
+
+// Sample an hemispherical direction with cosine distribution.
+@inlinable
+func sample_hemisphere_cos(_ normal: vec3f, _ ruv: vec2f) -> vec3f {
+    fatalError()
+}
+
+@inlinable
+func sample_hemisphere_cos_pdf(_ normal: vec3f, _ direction: vec3f) -> Float {
     fatalError()
 }
 
 // Sample an hemispherical direction with cosine power distribution.
 @inlinable
-func sample_hemisphere_cospower(exponent: Float, ruv: vec2f) -> vec3f {
+func sample_hemisphere_cospower(_ exponent: Float, _  ruv: vec2f) -> vec3f {
     fatalError()
 }
 
 @inlinable
-func sample_hemisphere_cospower_pdf(exponent: Float, direction: vec3f) -> Float {
+func sample_hemisphere_cospower_pdf(_ exponent: Float, _ direction: vec3f) -> Float {
     fatalError()
 }
 
 // Sample an hemispherical direction with cosine power distribution.
 @inlinable
-func sample_hemisphere_cospower(exponent: Float, normal: vec3f, ruv: vec2f) -> vec3f {
+func sample_hemisphere_cospower(_ exponent: Float, _ normal: vec3f, _ ruv: vec2f) -> vec3f {
     fatalError()
 }
 
 @inlinable
-func sample_hemisphere_cospower_pdf(exponent: Float, normal: vec3f, direction: vec3f) -> Float {
+func sample_hemisphere_cospower_pdf(_ exponent: Float, _ normal: vec3f, _ direction: vec3f) -> Float {
     fatalError()
 }
 
 // Sample a point uniformly on a disk.
 @inlinable
-func sample_disk(ruv: vec2f) -> vec2f {
+func sample_disk(_ ruv: vec2f) -> vec2f {
     fatalError()
 }
 
 @inlinable
-func sample_disk_pdf(point: vec2f) -> Float {
+func sample_disk_pdf(_ point: vec2f) -> Float {
     fatalError()
 }
 
 // Sample a point uniformly on a cylinder, without caps.
 @inlinable
-func sample_cylinder(ruv: vec2f) -> vec3f {
+func sample_cylinder(_ ruv: vec2f) -> vec3f {
     fatalError()
 }
 
 @inlinable
-func sample_cylinder_pdf(point: vec3f) -> Float {
+func sample_cylinder_pdf(_ point: vec3f) -> Float {
     fatalError()
 }
 
 // Sample a point uniformly on a triangle returning the baricentric coordinates.
 @inlinable
-func sample_triangle(ruv: vec2f) -> vec2f {
+func sample_triangle(_ ruv: vec2f) -> vec2f {
     fatalError()
 }
 
 // Sample a point uniformly on a triangle.
 @inlinable
-func sample_triangle(p0: vec3f, p1: vec3f, p2: vec3f, ruv: vec2f) -> vec3f {
+func sample_triangle(_ p0: vec3f, _  p1: vec3f, _ p2: vec3f, _ ruv: vec2f) -> vec3f {
     fatalError()
 }
 
 // Pdf for uniform triangle sampling, i.e. triangle area.
 @inlinable
-func sample_triangle_pdf(p0: vec3f, p1: vec3f, p2: vec3f) -> Float {
+func sample_triangle_pdf(_ p0: vec3f, _ p1: vec3f, _ p2: vec3f) -> Float {
     fatalError()
 }
 
 // Sample an index with uniform distribution.
 @inlinable
-func sample_uniform(size: Int, r: Float) -> Int {
+func sample_uniform(_ size: Int, _ r: Float) -> Int {
     fatalError()
 }
 
 @inlinable
-func sample_uniform_pdf(size: Int) -> Float {
+func sample_uniform_pdf(_ size: Int) -> Float {
     fatalError()
 }
 
 // Sample an index with uniform distribution.
 @inlinable
-func sample_uniform(elements: [Float], r: Float) -> Float {
+func sample_uniform(_ elements: [Float], _ r: Float) -> Float {
     fatalError()
 }
 
 @inlinable
-func sample_uniform_pdf(elements: [Float]) -> Float {
+func sample_uniform_pdf(_ elements: [Float]) -> Float {
     fatalError()
 }
 
 // Sample a discrete distribution represented by its cdf.
 @inlinable
-func sample_discrete(cdf: [Float], r: Float) -> Int {
+func sample_discrete(_ cdf: [Float], _ r: Float) -> Int {
     fatalError()
 }
 
 // Pdf for uniform discrete distribution sampling.
 @inlinable
-func sample_discrete_pdf(cdf: [Float], idx: Int) -> Float {
+func sample_discrete_pdf(_ cdf: [Float], _ idx: Int) -> Float {
     fatalError()
 }
