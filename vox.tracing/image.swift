bc889779f43a4bfea3badee6aee6b30c0507cc11
yangfeng <yangfengzzz@hotmail.com>
2021-09-06 22:07:18 +0800

    add image generator

--- vox.tracing/image.swift
+++ vox.tracing/image.swift
@@ -11,9 +11,36 @@ import Foundation
 // -----------------------------------------------------------------------------
 //MARK:- EXAMPLE IMAGES
 // -----------------------------------------------------------------------------
+// Comvert a bump map to a normal map.
+func bump_to_normal(_ normalmap: inout image_data, _ bumpmap: image_data, _ scale: Float) {
+    let width = bumpmap.width, height = bumpmap.height
+    if (normalmap.width != bumpmap.width || normalmap.height != bumpmap.height) {
+        normalmap = make_image(width, height, bumpmap.linear)
+    }
+    let dx = 1.0 / Float(width), dy = 1.0 / Float(height)
+    for j in 0..<height {
+        for i in 0..<width {
+            let i1 = (i + 1) % width, j1 = (j + 1) % height
+            let p00 = bumpmap.pixels[j * bumpmap.width + i],
+                    p10 = bumpmap.pixels[j * bumpmap.width + i1],
+                    p01 = bumpmap.pixels[j1 * bumpmap.width + i]
+            let g00 = (p00.x + p00.y + p00.z) / 3
+            let g01 = (p01.x + p01.y + p01.z) / 3
+            let g10 = (p10.x + p10.y + p10.z) / 3
+            var normal = vec3f(scale * (g00 - g10) / dx, scale * (g00 - g01) / dy, 1.0)
+            normal.y = -normal.y  // make green pointing up, even if y axis
+            // points down
+            normal = normalize(normal) * 0.5 + vec3f(0.5, 0.5, 0.5)
+            set_pixel(&normalmap, i, j, [normal.x, normal.y, normal.z, 1])
+        }
+    }
+}
+
 // Convert a bump map to a normal map. All linear color spaces.
-func bump_to_normal(_ image: image_data, _ scale: Float = 1) -> image_data {
-    fatalError()
+func bump_to_normal(_ bumpmap: image_data, _ scale: Float = 1) -> image_data {
+    var normalmap = make_image(bumpmap.width, bumpmap.height, bumpmap.linear)
+    bump_to_normal(&normalmap, bumpmap, scale)
+    return normalmap
 }
 
 // Convert a bump map to a normal map. All linear color spaces.
@@ -31,6 +58,18 @@ func add_border(_ img: image_data, _ width: Float, _ color: vec4f = [0, 0, 0, 1]
 func add_border(_ pixels: inout [vec4f], _ source: [vec4f], _  width: Int,
                 _ height: Int, _  thickness: Float, _ color: vec4f = [0, 0, 0, 1]) {
     fatalError()
+}
+
+func make_proc_image(_ width: Int, _ height: Int, _ linear: Bool, _ shader: (_ uv: vec2f) -> vec4f) -> image_data {
+    var image = make_image(width, height, linear)
+    let scale = 1.0 / Float(max(width, height))
+    for j in 0..<height {
+        for i in 0..<width {
+            let uv = vec2f(Float(i) * scale, Float(j) * scale)
+            image.pixels[j * width + i] = shader(uv)
+        }
+    }
+    return image
 }
 
 // -----------------------------------------------------------------------------
