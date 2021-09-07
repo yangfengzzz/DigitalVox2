90592353ebfc60af843c29c79c3cddd13216600f
yangfeng <yangfengzzz@hotmail.com>
2021-09-07 19:52:41 +0800

    finish texture and camera

--- vox.tracing/camera_data.swift
+++ vox.tracing/camera_data.swift
@@ -32,5 +32,31 @@ struct camera_data {
 
 // Generates a ray from a camera.
 func eval_camera(_ camera: camera_data, _ image_uv: vec2f, _ lens_uv: vec2f) -> ray3f {
-    fatalError()
+    let film = camera.aspect >= 1
+            ? vec2f(camera.film, camera.film / camera.aspect)
+            : vec2f(camera.film * camera.aspect, camera.film)
+    if (!camera.orthographic) {
+        let q = vec3f(film.x * (0.5 - image_uv.x), film.y * (image_uv.y - 0.5), camera.lens)
+        // ray direction through the lens center
+        let dc = -normalize(q)
+        // point on the lens
+        let e = vec3f(lens_uv.x * camera.aperture / 2, lens_uv.y * camera.aperture / 2, 0)
+        // point on the focus plane
+        let p = dc * camera.focus / abs(dc.z)
+        // correct ray direction to account for camera focusing
+        let d = normalize(p - e)
+        // done
+        return ray3f(transform_point(camera.frame, e), transform_direction(camera.frame, d))
+    } else {
+        let scale = 1 / camera.lens
+        let q = vec3f(film.x * (0.5 - image_uv.x) * scale, film.y * (image_uv.y - 0.5) * scale, camera.lens)
+        // point on the lens
+        let e = vec3f(-q.x, -q.y, 0) + vec3f(lens_uv.x * camera.aperture / 2, lens_uv.y * camera.aperture / 2, 0)
+        // point on the focus plane
+        let p = vec3f(-q.x, -q.y, -camera.focus)
+        // correct ray direction to account for camera focusing
+        let d = normalize(p - e)
+        // done
+        return ray3f(transform_point(camera.frame, e), transform_direction(camera.frame, d))
+    }
 }
--- vox.tracing/texture_data.swift
+++ vox.tracing/texture_data.swift
@@ -21,21 +21,77 @@ struct texture_data {
 func eval_texture(_ texture: texture_data, _ uv: vec2f,
                   _ as_linear: Bool = false, _ no_interpolation: Bool = false,
                   _ clamp_to_edge: Bool = false) -> vec4f {
-    fatalError()
+    if (texture.width == 0 || texture.height == 0) {
+        return [0, 0, 0, 0]
+    }
+
+    // get texture width/height
+    let size = vec2i(texture.width, texture.height)
+
+    // get coordinates normalized for tiling
+    var s: Float = 0.0, t: Float = 0.0
+    if (clamp_to_edge) {
+        s = simd_clamp(uv.x, 0.0, 1.0) * Float(size.x)
+        t = simd_clamp(uv.y, 0.0, 1.0) * Float(size.y)
+    } else {
+        s = fmod(uv.x, 1.0) * Float(size.x)
+        if (s < 0) {
+            s += Float(size.x)
+        }
+        t = fmod(uv.y, 1.0) * Float(size.y)
+        if (t < 0) {
+            t += Float(size.y)
+        }
+    }
+
+    // get image coordinates and residuals
+    let i = Int(simd_clamp(s, 0, Float(size.x) - 1)), j = Int(simd_clamp(t, 0, Float(size.y) - 1))
+    let ii = (i + 1) % size.x, jj = (j + 1) % size.y
+    let u = s - Float(i), v = t - Float(j)
+
+    // handle interpolation
+    if (no_interpolation) {
+        return lookup_texture(texture, i, j, as_linear)
+    } else {
+        var result = lookup_texture(texture, i, j, as_linear) * (1 - u) * (1 - v)
+        result += lookup_texture(texture, i, jj, as_linear) * (1 - u) * v
+        result += lookup_texture(texture, ii, j, as_linear) * u * (1 - v)
+        return result + lookup_texture(texture, ii, jj, as_linear) * u * v
+    }
 }
 
 func eval_texture(_ scene: scene_data, _ texture: Int, _ uv: vec2f,
-                  _ as_linear: Bool = false, _ no_interpolation: Bool = false,
+                  _ ldr_as_linear: Bool = false, _ no_interpolation: Bool = false,
                   _ clamp_to_edge: Bool = false) -> vec4f {
-    fatalError()
+    if (texture == invalidid) {
+        return [1, 1, 1, 1]
+    }
+    return eval_texture(scene.textures[texture], uv, ldr_as_linear, no_interpolation)
 }
 
 // pixel access
 func lookup_texture(_ texture: texture_data, _ i: Int, _ j: Int, _ as_linear: Bool = false) -> vec4f {
-    fatalError()
+    var color = vec4f(0, 0, 0, 0)
+    if (!texture.pixelsf.isEmpty) {
+        color = texture.pixelsf[j * texture.width + i]
+    } else {
+        color = byte_to_float(texture.pixelsb[j * texture.width + i])
+    }
+    if (as_linear && !texture.linear) {
+        return srgb_to_rgb(color)
+    } else {
+        return color
+    }
 }
 
 // conversion from image
 func image_to_texture(_ image: image_data) -> texture_data {
-    fatalError()
+    var texture = texture_data(width: image.width, height: image.height, linear: image.linear, pixelsf: [], pixelsb: [])
+    if (image.linear) {
+        texture.pixelsf = image.pixels
+    } else {
+        texture.pixelsb = .init(repeating: vec4b(), count: image.pixels.count)
+        float_to_byte(&texture.pixelsb, image.pixels)
+    }
+    return texture
 }
