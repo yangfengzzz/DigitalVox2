06663d4fda8e717b73fc922dadaf0886cc2edcb6
yangfeng <yangfengzzz@hotmail.com>
2021-09-21 08:28:45 +0800

    make textue loader as extension of assets

--- vox.render/material/SimpleMaterial.swift
+++ vox.render/material/SimpleMaterial.swift
@@ -9,6 +9,23 @@ import Foundation
 
 /// Simple Material.
 class SimpleMaterial: BaseMaterial {
+    private static var _baseTextureProp = Shader.getPropertyByName("u_diffuseTexture")
+    
+    /// Base texture.
+    var baseTexture: Texture2D? {
+        get {
+            return (shaderData.getTexture(SimpleMaterial._baseTextureProp) as! Texture2D)
+        }
+        set {
+            shaderData.setTexture(SimpleMaterial._baseTextureProp, newValue!)
+            if (newValue != nil) {
+                shaderData.enableMacro(DIFFUSE_TEXTURE)
+            } else {
+                shaderData.disableMacro(DIFFUSE_TEXTURE)
+            }
+        }
+    }
+    
     init(_ engine: Engine) {
         super.init(engine, Shader.find("simple")!)
     }
