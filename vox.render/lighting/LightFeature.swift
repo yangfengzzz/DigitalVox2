5e47eb19630174d85b3ff08f68ea762f41066621
yangfengzzz <yangfengzzz@hotmail.com>
2021-09-27 13:09:51 +0800

    solver light faeture problem

--- vox.render/lighting/LightFeature.swift
+++ vox.render/lighting/LightFeature.swift
@@ -11,6 +11,10 @@ import Foundation
 class LightFeature {
     var visibleLights: [Light] = []
 
+    var directLightCount: Int = 0
+    var pointLightCount: Int = 0
+    var spotLightCount: Int = 0
+
     /// Register a light object to the current scene.
     /// - Parameter light: light
     func attachRenderLight(light: Light) {
@@ -35,9 +39,9 @@ class LightFeature {
 
     internal func _updateShaderData(shaderData: ShaderData) {
         /// ambientLight and envMapLight only use the last one in the scene
-        var directLightCount:Int = 0
-        var pointLightCount:Int = 0
-        var spotLightCount:Int = 0
+        directLightCount = 0
+        pointLightCount = 0
+        spotLightCount = 0
 
         let lights = visibleLights
         for i in 0..<lights.count {
@@ -56,21 +60,27 @@ class LightFeature {
 
         if (directLightCount != 0) {
             DirectLight._updateShaderData(shaderData)
-            shaderData.enableMacro(DIRECT_LIGHT_COUNT, (&directLightCount, .int))
+            withUnsafePointer(to: &directLightCount) { pointer in
+                shaderData.enableMacro(DIRECT_LIGHT_COUNT, (UnsafeRawPointer(pointer), .int))
+            }
         } else {
             shaderData.disableMacro(DIRECT_LIGHT_COUNT)
         }
 
         if (pointLightCount != 0) {
             PointLight._updateShaderData(shaderData)
-            shaderData.enableMacro(POINT_LIGHT_COUNT, (&pointLightCount, .int))
+            withUnsafePointer(to: &pointLightCount) { pointer in
+                shaderData.enableMacro(POINT_LIGHT_COUNT, (UnsafeRawPointer(pointer), .int))
+            }
         } else {
             shaderData.disableMacro(POINT_LIGHT_COUNT)
         }
 
         if (spotLightCount != 0) {
             SpotLight._updateShaderData(shaderData)
-            shaderData.enableMacro(SPOT_LIGHT_COUNT, (&spotLightCount, .int))
+            withUnsafePointer(to: &spotLightCount) { pointer in
+                shaderData.enableMacro(SPOT_LIGHT_COUNT, (UnsafeRawPointer(pointer), .int))
+            }
         } else {
             shaderData.disableMacro(SPOT_LIGHT_COUNT)
         }
