85021432b2943175d3afa3253d16261fe01dad82
yangfengzzz <yangfengzzz@hotmail.com>
2021-10-27 21:05:24 +0800

    preprare to refactor physics

---
+++ vox.render/physics/enums/ColliderShapeUpAxis.swift
@@ -0,0 +1,18 @@
+//
+//  ColliderShapeUpAxis.swift
+//  vox.render
+//
+//  Created by 杨丰 on 2021/10/27.
+//
+
+import Foundation
+
+/// The up axis of the collider shape.
+enum ColliderShapeUpAxis {
+    /// Up axis is X.
+    case X
+    /// Up axis is Y.
+    case Y
+    /// Up axis is Z.
+    case Z
+}
