2e41f761f4568808e3022f4edd436fded1abe45e
yangfengzzz <yangfengzzz@hotmail.com>
2021-10-28 11:22:15 +0800

    can't use catalyst to link physx try to use macos versoin only

---
+++ vox.render/physics-physx/CPxPhysics.h
@@ -0,0 +1,17 @@
+//
+//  CPxPhysics.h
+//  DigitalVox2
+//
+//  Created by 杨丰 on 2021/10/28.
+//
+
+#ifndef CPxPhysics_h
+#define CPxPhysics_h
+
+#import <Foundation/Foundation.h>
+
+@interface CPxPhysics : NSObject
+
+@end
+
+#endif /* CPxPhysics_h */
---
+++ vox.render/physics-physx/CPxPhysics.mm
@@ -0,0 +1,35 @@
+//
+//  CPxPhysics.m
+//  vox.render
+//
+//  Created by 杨丰 on 2021/10/28.
+//
+
+#import "CPxPhysics.h"
+#import "PxPhysicsAPI.h"
+
+using namespace physx;
+
+@implementation CPxPhysics {
+    PxPhysics* _physics;
+}
+
+// MARK: - Initialization
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [self initializePhysics];
+    }
+    return self;
+}
+
+- (void)initializePhysics {
+//    PxDefaultAllocator        gAllocator;
+//    PxDefaultErrorCallback    gErrorCallback;
+//    physx::PxFoundation* gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
+//
+//    _physics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), false, NULL);
+}
+
+@end
