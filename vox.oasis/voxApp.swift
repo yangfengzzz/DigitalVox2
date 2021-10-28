7a25383d2df430cf9b5714a1530efe5f81aa9108
yangfengzzz <yangfengzzz@hotmail.com>
2021-10-28 11:26:28 +0800

    add vox.oasis

---
+++ vox.oasis/voxApp.swift
@@ -0,0 +1,17 @@
+//
+//  vox_oasisApp.swift
+//  vox.oasis
+//
+//  Created by 杨丰 on 2021/10/28.
+//
+
+import SwiftUI
+
+@main
+struct voxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
