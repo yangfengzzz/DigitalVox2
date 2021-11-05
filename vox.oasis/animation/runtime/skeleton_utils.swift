5eecb6aa0cc8c758de4641794b3c2dae0f560733
yangfengzzz <yangfengzzz@hotmail.com>
2021-11-05 16:06:14 +0800

    move skeleton utility into skeleton_utils

--- vox.oasis/animation/runtime/skeleton_utils.swift
+++ vox.oasis/animation/runtime/skeleton_utils.swift
@@ -80,3 +80,220 @@ func iterateJointsDFReverse(_ _skeleton: SoaSkeleton, _ _fct: (Int, Int) -> Void
         _fct(i, parents[i])
     }
 }
+
+//MARK: - Render Utility
+func fillPostureUniforms(_ _skeleton: SoaSkeleton,
+                         _ _matrices: [matrix_float4x4],
+                         _ _uniforms: inout [matrix_float4x4]) {
+    // Prepares computation constants.
+    let num_joints = _skeleton.num_joints()
+    let parents = _skeleton.joint_parents()
+
+    var instances = 0
+    for i in 0..<num_joints {
+        // Root isn't rendered.
+        let parent_id = parents[i]
+        if (parent_id == SoaSkeleton.Constants.kNoParent.rawValue) {
+            continue
+        }
+
+        // Selects joint matrices.
+        let parent = _matrices[parent_id]
+        let current = _matrices[i]
+
+        // Copy parent joint's raw matrix, to render a bone between the parent and current matrix.
+        _uniforms.append(parent)
+
+        // Set bone direction (bone_dir). The shader expects to find it at index
+        // [3,7,11] of the matrix.
+        // Index 15 is used to store whether a bone should be rendered,
+        // otherwise it's a leaf.
+        var bone_dir: [Float] = [0, 0, 0, 0]
+        storePtrU(current.columns.3 - parent.columns.3, &bone_dir)
+        _uniforms[instances].columns.0.w = bone_dir[0]
+        _uniforms[instances].columns.1.w = bone_dir[1]
+        _uniforms[instances].columns.2.w = bone_dir[2]
+        _uniforms[instances].columns.3.w = 1.0 // Enables bone rendering.
+
+        // Next instance.
+        instances += 1
+
+        // Only the joint is rendered for leaves, the bone model isn't.
+        if (isLeaf(_skeleton, i)) {
+            // Copy current joint's raw matrix.
+            _uniforms.append(current)
+
+            // Re-use bone_dir to fix the size of the leaf (same as previous bone).
+            // The shader expects to find it at index [3,7,11] of the matrix.
+            _uniforms[instances].columns.0.w = bone_dir[0]
+            _uniforms[instances].columns.1.w = bone_dir[1]
+            _uniforms[instances].columns.2.w = bone_dir[2]
+            _uniforms[instances].columns.3.w = 0.0  // Disables bone rendering.
+            instances += 1
+        }
+    }
+}
+
+func createBoneMesh(_ engine: Engine) -> ModelMesh {
+    let kInter: Float = 0.2
+    let mesh = ModelMesh(engine)
+    var positions: [Vector3] = [Vector3](repeating: Vector3(), count: 6)
+    positions[0] = Vector3(1.0, 0.0, 0.0)
+    positions[1] = Vector3(kInter, 0.1, 0.1)
+    positions[2] = Vector3(kInter, 0.1, -0.1)
+    positions[3] = Vector3(kInter, -0.1, -0.1)
+    positions[4] = Vector3(kInter, -0.1, 0.1)
+    positions[5] = Vector3(0.0, 0.0, 0.0)
+
+    let pos: [VecFloat3] = [
+        VecFloat3(1.0, 0.0, 0.0), VecFloat3(kInter, 0.1, 0.1),
+        VecFloat3(kInter, 0.1, -0.1), VecFloat3(kInter, -0.1, -0.1),
+        VecFloat3(kInter, -0.1, 0.1), VecFloat3(0.0, 0.0, 0.0)
+    ]
+
+    let n: [VecFloat3] = [
+        normalize(cross(pos[2] - pos[1], pos[2] - pos[0])),
+        normalize(cross(pos[1] - pos[2], pos[1] - pos[5])),
+        normalize(cross(pos[3] - pos[2], pos[3] - pos[0])),
+        normalize(cross(pos[2] - pos[3], pos[2] - pos[5])),
+        normalize(cross(pos[4] - pos[3], pos[4] - pos[0])),
+        normalize(cross(pos[3] - pos[4], pos[3] - pos[5])),
+        normalize(cross(pos[1] - pos[4], pos[1] - pos[0])),
+        normalize(cross(pos[4] - pos[1], pos[4] - pos[5]))
+    ]
+    let vertex_n = [
+        (n[0] + n[2] + n[4] + n[6]) * 0.25,
+        (n[0] + n[1] + n[6] + n[7]) * 0.25,
+        (n[0] + n[1] + n[2] + n[3]) * 0.25,
+        (n[2] + n[3] + n[4] + n[5]) * 0.25,
+        (n[4] + n[5] + n[6] + n[7]) * 0.25,
+        (n[1] + n[3] + n[5] + n[7]) * 0.25,
+    ]
+
+    let normals: [Vector3] = [
+        Vector3(vertex_n[0].x, vertex_n[0].y, vertex_n[0].z),
+        Vector3(vertex_n[1].x, vertex_n[1].y, vertex_n[1].z),
+        Vector3(vertex_n[2].x, vertex_n[2].y, vertex_n[2].z),
+        Vector3(vertex_n[3].x, vertex_n[3].y, vertex_n[3].z),
+        Vector3(vertex_n[4].x, vertex_n[4].y, vertex_n[4].z),
+        Vector3(vertex_n[5].x, vertex_n[5].y, vertex_n[5].z),
+    ]
+
+    let white = Color(1, 1, 1, 1)
+    let colors: [Color] = [
+        white,
+        white,
+        white,
+        white,
+        white,
+        white
+    ]
+
+    var indices = [UInt32](repeating: 0, count: 24)
+    indices[0] = 0
+    indices[1] = 2
+    indices[2] = 1
+    indices[3] = 5
+    indices[4] = 1
+    indices[5] = 2
+
+    indices[6] = 0
+    indices[7] = 3
+    indices[8] = 2
+    indices[9] = 5
+    indices[10] = 2
+    indices[11] = 3
+
+    indices[12] = 0
+    indices[13] = 4
+    indices[14] = 3
+    indices[15] = 5
+    indices[16] = 3
+    indices[17] = 4
+
+    indices[18] = 0
+    indices[19] = 1
+    indices[20] = 4
+    indices[21] = 5
+    indices[22] = 4
+    indices[23] = 1
+
+    mesh.setPositions(positions: positions)
+    mesh.setNormals(normals: normals)
+    mesh.setColors(colors: colors)
+    mesh.uploadData(true)
+    let indexBuffer = engine._hardwareRenderer.device.makeBuffer(bytes: indices,
+            length: indices.count * MemoryLayout<UInt32>.stride,
+            options: .storageModeShared)
+    _ = mesh.addSubMesh(MeshBuffer(indexBuffer!, indices.count * MemoryLayout<UInt32>.stride, .index),
+            .uint32, indices.count, .triangle)
+
+    return mesh
+}
+
+func createJointMesh(_ engine: Engine) -> ModelMesh {
+    let kNumSlices = 20
+    let kNumPointsPerCircle = kNumSlices + 1
+    let kNumPointsYZ = kNumPointsPerCircle
+    let kNumPointsXY = kNumPointsPerCircle + kNumPointsPerCircle / 4
+    let kNumPointsXZ = kNumPointsPerCircle
+    let kRadius: Float = 0.2  // Radius multiplier.
+    let red = Color(1, 0, 0, 1)
+    let green = Color(0, 1, 0, 1)
+    let blue = Color(0, 0, 1, 1)
+
+    var positions: [Vector3] = []
+    var normals: [Vector3] = []
+    var colors: [Color] = []
+    var indices: [UInt32] = []
+
+    var index: UInt32 = 0
+    for j in 0..<kNumPointsYZ { // YZ plan.
+        indices.append(index)
+        let angle = Float(j) * k2Pi / Float(kNumSlices)
+        let s = sinf(angle), c = cosf(angle)
+        positions.append(Vector3(0.0, c * kRadius, s * kRadius))
+        normals.append(Vector3(0.0, c, s))
+        colors.append(red)
+        index += 1
+        indices.append(index)
+    }
+    indices[indices.count - 1] = 0
+
+    for j in 0..<kNumPointsXY { // XY plan.
+        indices.append(index)
+        let angle = Float(j) * k2Pi / Float(kNumSlices)
+        let s = sinf(angle), c = cosf(angle)
+        positions.append(Vector3(s * kRadius, c * kRadius, 0.0))
+        normals.append(Vector3(s, c, 0.0))
+        colors.append(blue)
+        index += 1
+        indices.append(index)
+    }
+    indices[indices.count - 1] = UInt32(kNumPointsYZ)
+
+    for j in 0..<kNumPointsXZ { // XZ plan.
+        indices.append(index)
+        let angle = Float(j) * k2Pi / Float(kNumSlices)
+        let s = sinf(angle), c = cosf(angle)
+        positions.append(Vector3(c * kRadius, 0.0, -s * kRadius))
+        normals.append(Vector3(c, 0.0, -s))
+        colors.append(green)
+        index += 1
+        indices.append(index)
+    }
+    indices[indices.count - 1] = UInt32(kNumPointsYZ + kNumPointsXY)
+
+    let mesh = ModelMesh(engine)
+    mesh.setPositions(positions: positions)
+    mesh.setNormals(normals: normals)
+    mesh.setColors(colors: colors)
+    mesh.uploadData(true)
+    let indexBuffer = engine._hardwareRenderer.device.makeBuffer(bytes: indices,
+            length: indices.count * MemoryLayout<UInt32>.stride,
+            options: .storageModeShared)
+    _ = mesh.addSubMesh(MeshBuffer(indexBuffer!, indices.count * MemoryLayout<UInt32>.stride, .index),
+            .uint32, indices.count, .line)
+    return mesh
+}
+
