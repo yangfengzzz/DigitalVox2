34c427525f2be2a1c451eeb9d3ac29ee3aed3abd
yangfengzzz <yangfengzzz@hotmail.com>
2021-11-05 12:54:37 +0800

    add bone shader and bone mesh

---
+++ vox.oasis/BoneShader.metal
@@ -0,0 +1,70 @@
+//
+//  BoneShader.metal
+//  vox.oasis
+//
+//  Created by 杨丰 on 2021/11/5.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+#import "function-constant.metal"
+
+float4x4 GetWorldMatrix(float4x4 joint) {
+    // Rebuilds bone properties.
+    // Bone length is set to zero to disable leaf rendering.
+    float is_bone = joint[3].w;
+    float3 bone_dir = float3(joint[0].w, joint[1].w, joint[2].w) * is_bone;
+    float bone_len = length(bone_dir);
+    
+    // Setup rendering world matrix.\n"
+    float dot1 = dot(joint[2].xyz, bone_dir);
+    float dot2 = dot(joint[0].xyz, bone_dir);
+    float3 binormal = abs(dot1) < abs(dot2) ? joint[2].xyz : joint[0].xyz;
+    
+    float4x4 world_matrix;
+    world_matrix[0] = float4(bone_dir, 0.);
+    world_matrix[1] = float4(bone_len * normalize(cross(binormal, bone_dir)), 0.);
+    world_matrix[2] = float4(bone_len * normalize(cross(bone_dir, world_matrix[1].xyz)), 0.);
+    world_matrix[3] = float4(joint[3].xyz, 1.);
+    return world_matrix;
+}
+
+struct VertexIn {
+    float3 a_position [[attribute(Position)]];
+    float3 a_normal [[attribute(Normal)]];
+};
+
+struct VertexOut {
+    float4 position [[position]];
+    float3 v_world_normal;
+};
+
+vertex VertexOut bone_vertex(const VertexIn in [[stage_in]],
+                             constant matrix_float4x4 &u_MVPMat [[buffer(5)]],
+                             constant matrix_float4x4 &joint [[buffer(6)]]) {
+    VertexOut out;
+    
+    float4x4 world_matrix = GetWorldMatrix(joint);
+    float4 pos = float4(in.a_position.xyz, 1.);
+    out.position = u_MVPMat * world_matrix * pos;
+    float3x3 cross_matrix = float3x3(cross(world_matrix[1].xyz, world_matrix[2].xyz),
+                                     cross(world_matrix[2].xyz, world_matrix[0].xyz),
+                                     cross(world_matrix[0].xyz, world_matrix[1].xyz));
+    float invdet = 1.0 / dot(cross_matrix[2], world_matrix[2].xyz);
+    float3x3 normal_matrix = cross_matrix * invdet;
+    out.v_world_normal = normal_matrix * in.a_normal;
+    return out;
+}
+
+float4 GetAmbient(float3 _world_normal) {
+    float3 normal = normalize(_world_normal);
+    float3 alpha = (normal + 1.) * .5;
+    float2 bt = mix(float2(.3, .7), float2(.4, .8), alpha.xz);
+    float3 ambient = mix(float3(bt.x, .3, bt.x), float3(bt.y, .8, bt.y), alpha.y);
+    return float4(ambient, 1.);
+}
+
+fragment float4 bone_fragment(VertexOut in [[stage_in]]) {
+    float4 ambient = GetAmbient(in.v_world_normal);
+    return ambient * float4(0.5, 0.5, 0.5, 1);
+}
--- vox.oasis/views/SkeletonView.swift
+++ vox.oasis/views/SkeletonView.swift
@@ -7,6 +7,92 @@
 
 import SwiftUI
 
+ func createBone(_ engine: Engine) -> ModelMesh {
+     let kInter:Float = 0.2
+     let mesh = ModelMesh(engine)
+     let positions: [Vector3] = [Vector3](repeating: Vector3(), count: 6)
+     _ = positions[0].setValue(x: 1.0, y: 0.0, z: 0.0)
+     _ = positions[1].setValue(x: kInter, y: 0.1, z: 0.1)
+     _ = positions[2].setValue(x: kInter, y: 0.1, z: -0.1)
+     _ = positions[3].setValue(x: kInter, y: -0.1, z: -0.1)
+     _ = positions[4].setValue(x: kInter, y: -0.1, z: 0.1)
+     _ = positions[5].setValue(x: 0.0, y: 0.0, z: 0.0)
+     
+     let pos:[VecFloat3] = [
+         VecFloat3(1.0, 0.0, 0.0),     VecFloat3(kInter, 0.1, 0.1),
+         VecFloat3(kInter, 0.1, -0.1), VecFloat3(kInter, -0.1, -0.1),
+         VecFloat3(kInter, -0.1, 0.1), VecFloat3(0.0, 0.0, 0.0)
+         ]
+     
+     let n:[VecFloat3] = [
+            normalize(cross(pos[2] - pos[1], pos[2] - pos[0])),
+            normalize(cross(pos[1] - pos[2], pos[1] - pos[5])),
+            normalize(cross(pos[3] - pos[2], pos[3] - pos[0])),
+            normalize(cross(pos[2] - pos[3], pos[2] - pos[5])),
+            normalize(cross(pos[4] - pos[3], pos[4] - pos[0])),
+            normalize(cross(pos[3] - pos[4], pos[3] - pos[5])),
+            normalize(cross(pos[1] - pos[4], pos[1] - pos[0])),
+            normalize(cross(pos[4] - pos[1], pos[4] - pos[5]))
+            ]
+     let vertex_n = [
+        (n[0] + n[2] + n[4] + n[6]) * 0.25,
+        (n[0] + n[1] + n[6] + n[7]) * 0.25,
+        (n[0] + n[1] + n[2] + n[3]) * 0.25,
+        (n[2] + n[3] + n[4] + n[5]) * 0.25,
+        (n[4] + n[5] + n[6] + n[7]) * 0.25,
+        (n[1] + n[3] + n[5] + n[7]) * 0.25,
+     ]
+    
+     let normals: [Vector3] = [
+        Vector3(vertex_n[0].x, vertex_n[0].y, vertex_n[0].z),
+        Vector3(vertex_n[1].x, vertex_n[1].y, vertex_n[1].z),
+        Vector3(vertex_n[2].x, vertex_n[2].y, vertex_n[2].z),
+        Vector3(vertex_n[3].x, vertex_n[3].y, vertex_n[3].z),
+        Vector3(vertex_n[4].x, vertex_n[4].y, vertex_n[4].z),
+        Vector3(vertex_n[5].x, vertex_n[5].y, vertex_n[5].z),
+     ]
+     
+     var indices = [UInt32](repeating: 0, count: 24)
+     indices[0] = 0
+     indices[1] = 2
+     indices[2] = 1
+     indices[3] = 5
+     indices[4] = 1
+     indices[5] = 2
+     
+     indices[6] = 0
+     indices[7] = 3
+     indices[8] = 2
+     indices[9] = 5
+     indices[10] = 2
+     indices[11] = 3
+     
+     indices[12] = 0
+     indices[13] = 4
+     indices[14] = 3
+     indices[15] = 5
+     indices[16] = 3
+     indices[17] = 4
+     
+     indices[18] = 0
+     indices[19] = 1
+     indices[20] = 4
+     indices[21] = 5
+     indices[22] = 4
+     indices[23] = 1
+     
+     mesh.setPositions(positions: positions)
+     mesh.setNormals(normals: normals)
+     mesh.uploadData(true)
+     let indexBuffer = engine._hardwareRenderer.device.makeBuffer(bytes: indices,
+             length: indices.count * MemoryLayout<UInt32>.stride,
+             options: .storageModeShared)
+     _ = mesh.addSubMesh(MeshBuffer(indexBuffer!, indices.count * MemoryLayout<UInt32>.stride, .index),
+             .uint32, indices.count, .triangle)
+     
+     return mesh
+ }
+
 struct SkeletonView: View {
     let canvas: Canvas
     let engine: Engine
