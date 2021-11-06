b3c2229d743658ebc9fb514bca56b058255e3964
yangfeng <yangfengzzz@hotmail.com>
2021-11-06 08:15:44 +0800

    add animation_utils

--- vox.oasis/animation/runtime/animation_utils.swift
+++ vox.oasis/animation/runtime/animation_utils.swift
@@ -9,19 +9,19 @@ import Foundation
 
 // Count translation, rotation or scale keyframes for a given track number. Use
 // a negative _track value to count all tracks.
-func CountTranslationKeyframes(_animation: Animation, _track: Int = -1) -> Int {
-    CountKeyframesImpl(_animation.translations(), _track)
+func countTranslationKeyframes(_ _animation: SoaAnimation, _ _track: Int = -1) -> Int {
+    countKeyframesImpl(_animation.translations(), _track)
 }
 
-func CountRotationKeyframes(_animation: Animation, _track: Int = -1) -> Int {
-    CountKeyframesImpl(_animation.rotations(), _track)
+func countRotationKeyframes(_ _animation: SoaAnimation, _ _track: Int = -1) -> Int {
+    countKeyframesImpl(_animation.rotations(), _track)
 }
 
-func CountScaleKeyframes(_animation: Animation, _track: Int = -1) -> Int {
-    CountKeyframesImpl(_animation.scales(), _track)
+func countScaleKeyframes(_ _animation: SoaAnimation, _ _track: Int = -1) -> Int {
+    countKeyframesImpl(_animation.scales(), _track)
 }
 
-fileprivate func CountKeyframesImpl<_Key: KeyframeType>(_ _keys: ArraySlice<_Key>, _ _track: Int) -> Int {
+fileprivate func countKeyframesImpl<_Key: KeyframeType>(_ _keys: ArraySlice<_Key>, _ _track: Int) -> Int {
     if (_track < 0) {
         return _keys.count
     }
