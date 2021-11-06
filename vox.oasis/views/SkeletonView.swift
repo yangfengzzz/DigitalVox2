5bcab5106cc7a597b3ded37630ea5ba16cb59767
yangfeng <yangfengzzz@hotmail.com>
2021-11-06 21:55:02 +0800

    add animation controller

--- vox.oasis/views/SkeletonView.swift
+++ vox.oasis/views/SkeletonView.swift
@@ -222,6 +222,203 @@ func createAnimation(_ skeleton_: SoaSkeleton, _ _animation: inout RawAnimation)
     }
 }
 
+// Utility class that helps with controlling animation playback time. Time is
+// computed every update according to the dt given by the caller, playback speed
+// and "play" state.
+// Internally time is stored as a ratio in unit interval [0,1], as expected by
+// ozz runtime animation jobs.
+// OnGui function allows to tweak controller parameters through the application
+// Gui.
+class PlaybackController {
+    // Sets animation current time.
+    func set_time_ratio(_ _ratio: Float) {
+        previous_time_ratio_ = time_ratio_
+        if (loop_) {
+            // Wraps in the unit interval [0:1], even for negative values (the reason
+            // for using floorf).
+            time_ratio_ = _ratio - floorf(_ratio)
+        } else {
+            // Clamps in the unit interval [0:1].
+            time_ratio_ = simd_clamp(0.0, _ratio, 1.0)
+        }
+    }
+
+    // Gets animation current time.
+    func time_ratio() -> Float {
+        time_ratio_
+    }
+
+    // Gets animation time ratio of last update. Useful when the range between
+    // previous and current frame needs to pe processed.
+    func previous_time_ratio() -> Float {
+        previous_time_ratio_
+    }
+
+    // Sets playback speed.
+    func set_playback_speed(_ _speed: Float) {
+        playback_speed_ = _speed
+    }
+
+    // Gets playback speed.
+    func playback_speed() -> Float {
+        playback_speed_
+    }
+
+    // Sets loop modes. If true, animation time is always clamped between 0 and 1.
+    func set_loop(_ _loop: Bool) {
+        loop_ = _loop
+    }
+
+    // Gets loop mode.
+    func loop() -> Bool {
+        loop_
+    }
+
+    // Updates animation time if in "play" state, according to playback speed and
+    // given frame time _dt.
+    // Returns true if animation has looped during update
+    func update(_ _animation: SoaAnimation, _ _dt: Float) {
+        var new_time = time_ratio_
+
+        if (play_) {
+            new_time = time_ratio_ + _dt * playback_speed_ / _animation.duration()
+        }
+
+        // Must be called even if time doesn't change, in order to update previous
+        // frame time ratio. Uses set_time_ratio function in order to update
+        // previous_time_ an wrap time value in the unit interval (depending on loop
+        // mode).
+        set_time_ratio(new_time)
+    }
+
+    // Resets all parameters to their default value.
+    func reset() {
+        previous_time_ratio_ = 0.0
+        time_ratio_ = 0.0
+        playback_speed_ = 1.0
+        play_ = true
+    }
+
+    // Current animation time ratio, in the unit interval [0,1], where 0 is the
+    // beginning of the animation, 1 is the end.
+    private var time_ratio_: Float = 0.0
+
+    // Time ratio of the previous update.
+    private var previous_time_ratio_: Float = 0.0
+
+    // Playback speed, can be negative in order to play the animation backward.
+    private var playback_speed_: Float = 1.0
+
+    // Animation play mode state: play/pause.
+    private var play_: Bool = true
+
+    // Animation loop mode.
+    private var loop_: Bool = true
+}
+
+class AnimationScript: Script {
+    // Playback animation controller. This is a utility class that helps with
+    // controlling animation playback time.
+    let controller_ = PlaybackController()
+    // The millipede skeleton.
+    var skeleton_: SoaSkeleton!
+
+    // The millipede procedural walk animation.
+    var animation_: SoaAnimation!
+
+    // Sampling cache, as used by SamplingJob.
+    var cache_ = SamplingCache()
+
+    // Buffer of local transforms as sampled from animation_.
+    // These are shared between sampling output and local-to-model input.
+    var locals_: [SoaTransform] = []
+
+    // Buffer of model matrices (local-to-model output).
+    var models_: [matrix_float4x4] = []
+
+    var boneMesh: ModelMesh!
+    var jointMesh: ModelMesh!
+    let joint = Matrix()
+    var isFirst = true
+
+    var boneMtl: [BoneMaterial] = []
+    var jointMtl: [JointMaterial] = []
+
+    required init(_ entity: Entity) {
+        super.init(entity)
+        boneMesh = createBoneMesh(engine)
+        jointMesh = createJointMesh(engine)
+    }
+
+    func load(_ skeleton_: SoaSkeleton, _ animation_: SoaAnimation) {
+        self.skeleton_ = skeleton_
+        self.animation_ = animation_
+
+        // Allocates runtime buffers.
+        let num_soa_joints = skeleton_.num_soa_joints()
+        let num_joints = skeleton_.num_joints()
+        locals_ = [SoaTransform](repeating: SoaTransform.identity(), count: num_soa_joints)
+        models_ = [matrix_float4x4](repeating: matrix_float4x4.identity(), count: num_joints)
+
+        // Allocates a cache that matches new animation requirements.
+        cache_.resize(num_joints)
+    }
+
+    override func onUpdate(_ deltaTime: Float) {
+        // Updates current animation time
+        controller_.update(animation_, deltaTime)
+
+        // Samples animation at t = animation_time_.
+        let sampling_job = SamplingJob()
+        sampling_job.animation = animation_
+        sampling_job.cache = cache_
+        sampling_job.ratio = controller_.time_ratio()
+        if (!sampling_job.run(&locals_[...])) {
+            return
+        }
+
+        // Converts from local space to model space matrices.
+        let ltm_job = LocalToModelJob()
+        ltm_job.skeleton = skeleton_
+        ltm_job.input = locals_[...]
+        _ = ltm_job.run(&models_[...])
+
+        var joints: [matrix_float4x4] = []
+        fillPostureUniforms(skeleton_, models_, &joints)
+
+        // add bone renderer
+        for i in 0..<joints.count {
+            if isFirst {
+                let boneEntity = entity.createChild("bone\(i)")
+                let renderer: MeshRenderer = boneEntity.addComponent()
+                let mtl = BoneMaterial(engine)
+                boneMtl.append(mtl)
+                joint.elements = joints[i]
+                mtl.joint = joint
+                renderer.mesh = boneMesh
+                renderer.setMaterial(mtl)
+            } else {
+                joint.elements = joints[i]
+                boneMtl[i].joint = joint
+            }
+        }
+        for i in 0..<joints.count {
+            if isFirst {
+                let boneEntity = entity.createChild("joint\(i)")
+                let renderer: MeshRenderer = boneEntity.addComponent()
+                let mtl = JointMaterial(engine)
+                jointMtl.append(mtl)
+                joint.elements = joints[i]
+                mtl.joint = joint
+                renderer.mesh = jointMesh
+                renderer.setMaterial(mtl)
+            } else {
+                joint.elements = joints[i]
+                jointMtl[i].joint = joint
+            }
+        }
+    }
+}
 
 //MARK: - View
 struct SkeletonView: View {
@@ -270,48 +467,8 @@ struct SkeletonView: View {
             return
         }
 
-        // local to model
-        let num_joints = skeleton.num_joints()
-        if (num_joints == 0) {
-            return
-        }
-
-        // Reallocate matrix array if necessary.
-        var prealloc_models_ = [simd_float4x4](repeating: simd_float4x4(), count: num_joints)
-
-        // Compute model space bind pose.
-        let job = LocalToModelJob()
-        job.input = skeleton.joint_bind_poses()
-        job.skeleton = skeleton
-        if (!job.run(&prealloc_models_[...])) {
-            return
-        }
-
-        var joints: [matrix_float4x4] = []
-        fillPostureUniforms(skeleton, prealloc_models_, &joints)
-
-        // add bone renderer
-        let boneMesh = createBoneMesh(engine)
-        let jointMesh = createJointMesh(engine)
-        let joint = Matrix()
-        for i in 0..<joints.count {
-            let boneEntity = rootEntity.createChild("bone\(i)")
-            let renderer: MeshRenderer = boneEntity.addComponent()
-            let mtl = BoneMaterial(engine)
-            joint.elements = joints[i]
-            mtl.joint = joint
-            renderer.mesh = boneMesh
-            renderer.setMaterial(mtl)
-        }
-        for i in 0..<joints.count {
-            let boneEntity = rootEntity.createChild("joint\(i)")
-            let renderer: MeshRenderer = boneEntity.addComponent()
-            let mtl = JointMaterial(engine)
-            joint.elements = joints[i]
-            mtl.joint = joint
-            renderer.mesh = jointMesh
-            renderer.setMaterial(mtl)
-        }
+        let script: AnimationScript = rootEntity.addComponent()
+        script.load(skeleton, animation_)
     }
 
     var body: some View {
