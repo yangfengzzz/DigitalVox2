6b667bfe193a102f39c82b3fb56f4f0badb24ac2
yangfengzzz <yangfengzzz@hotmail.com>
2021-11-09 09:14:38 +0800

    add controller

--- vox.oasis/physics-physx/characterkinematic/CPxController.h
+++ vox.oasis/physics-physx/characterkinematic/CPxController.h
@@ -8,9 +8,58 @@
 #ifndef CPxController_h
 #define CPxController_h
 
-#import <Foundation/Foundation.h>
+#import "CPxControllerDesc.h"
+#import "CPxObstacle.h"
+#import "../CPxRigidDynamic.h"
+
+enum CPxControllerCollisionFlag {
+    //!< Character is colliding to the sides.
+    eCOLLISION_SIDES = (1 << 0),
+    //!< Character has collision above.
+    eCOLLISION_UP = (1 << 1),
+    //!< Character has collision below.
+    eCOLLISION_DOWN = (1 << 2)
+};
 
 @interface CPxController : NSObject
+
+- (enum CPxControllerShapeType)getType;
+
+- (uint8_t)move:(simd_float3)disp :(float)minDist :(float)elapsedTime;
+
+- (bool)setPosition:(simd_float3)position;
+
+- (simd_float3)getPosition;
+
+- (void)setFootPosition:(simd_float3)position;
+
+- (simd_float3)getFootPosition;
+
+- (CPxRigidDynamic *_Nonnull)getActor;
+
+- (void)setStepOffset:(float)offset;
+
+- (float)getStepOffset;
+
+- (void)setNonWalkableMode:(enum CPxControllerNonWalkableMode)flag;
+
+- (enum CPxControllerNonWalkableMode)getNonWalkableMode;
+
+- (float)getContactOffset;
+
+- (void)setContactOffset:(float)offset;
+
+- (simd_float3)getUpDirection;
+
+- (void)setUpDirection:(simd_float3)up;
+
+- (float)getSlopeLimit;
+
+- (void)setSlopeLimit:(float)slopeLimit;
+
+- (void)invalidateCache;
+
+- (void)resize:(float)height;
 
 @end
 
--- vox.oasis/physics-physx/characterkinematic/CPxController.mm
+++ vox.oasis/physics-physx/characterkinematic/CPxController.mm
@@ -7,7 +7,95 @@
 
 #import "CPxController.h"
 #import "CPxController+Internal.h"
+#import "../CPxRigidDynamic+Internal.h"
 
 @implementation CPxController
+
+- (instancetype)initWithController:(PxController *)controller {
+    self = [super init];
+    if (self) {
+        _c_controller = controller;
+    }
+    return self;
+}
+
+- (enum CPxControllerShapeType)getType {
+    return CPxControllerShapeType(_c_controller->getType());
+}
+
+- (uint8_t)move:(simd_float3)disp :(float)minDist :(float)elapsedTime {
+    return _c_controller->move(PxVec3(disp.x, disp.y, disp.z), minDist, elapsedTime, PxControllerFilters());
+}
+
+- (bool)setPosition:(simd_float3)position {
+    return _c_controller->setPosition(PxExtendedVec3(position.x, position.y, position.z));
+}
+
+- (simd_float3)getPosition {
+    PxExtendedVec3 pos = _c_controller->getPosition();
+    return simd_make_float3(pos.x, pos.y, pos.z);
+}
+
+- (void)setFootPosition:(simd_float3)position {
+    _c_controller->setFootPosition(PxExtendedVec3(position.x, position.y, position.z));
+}
+
+- (simd_float3)getFootPosition {
+    PxExtendedVec3 pos = _c_controller->getFootPosition();
+    return simd_make_float3(pos.x, pos.y, pos.z);
+}
+
+- (CPxRigidDynamic *_Nonnull)getActor {
+    return [[CPxRigidDynamic alloc] initWithDynamicActor:_c_controller->getActor()];
+}
+
+- (void)setStepOffset:(float)offset {
+    _c_controller->setStepOffset(offset);
+}
+
+- (float)getStepOffset {
+    return _c_controller->getStepOffset();
+}
+
+- (void)setNonWalkableMode:(enum CPxControllerNonWalkableMode)flag {
+    _c_controller->setNonWalkableMode(PxControllerNonWalkableMode::Enum(flag));
+}
+
+- (enum CPxControllerNonWalkableMode)getNonWalkableMode {
+    return CPxControllerNonWalkableMode(_c_controller->getNonWalkableMode());
+}
+
+- (float)getContactOffset {
+    return _c_controller->getContactOffset();
+}
+
+- (void)setContactOffset:(float)offset {
+    _c_controller->setContactOffset(offset);
+}
+
+- (simd_float3)getUpDirection {
+    PxVec3 pos = _c_controller->getUpDirection();
+    return simd_make_float3(pos.x, pos.y, pos.z);
+}
+
+- (void)setUpDirection:(simd_float3)up {
+    _c_controller->setUpDirection(PxVec3(up.x, up.y, up.z));
+}
+
+- (float)getSlopeLimit {
+    return _c_controller->getSlopeLimit();
+}
+
+- (void)setSlopeLimit:(float)slopeLimit {
+    _c_controller->setSlopeLimit(slopeLimit);
+}
+
+- (void)invalidateCache {
+    _c_controller->invalidateCache();
+}
+
+- (void)resize:(float)height {
+    _c_controller->resize(height);
+}
 
 @end
