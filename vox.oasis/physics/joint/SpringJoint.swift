ee006458825a083ed207a1f3696d0ef3f2b50c2b
yangfengzzz <yangfengzzz@hotmail.com>
2021-11-09 15:54:55 +0800

    turn more func into property

--- vox.oasis/physics/joint/SpringJoint.swift
+++ vox.oasis/physics/joint/SpringJoint.swift
@@ -7,33 +7,75 @@
 
 import Foundation
 
+enum SpringJointFlag: Int {
+    case MAX_DISTANCE_ENABLED = 2
+    case MIN_DISTANCE_ENABLED = 4
+    case SPRING_ENABLED = 8
+}
+
 class SpringJoint: Joint {
+    private var _minDistance: Float = 0
+    private var _maxDistance: Float = 0
+    private var _tolerance: Float = 0
+    private var _stiffness: Float = 0
+    private var _damping: Float = 0
+
     required init(_ entity: Entity) {
         super.init(entity)
         _nativeJoint = PhysicsManager._nativePhysics.createSpringJoint(nil, Vector3(), Quaternion(), nil, Vector3(), Quaternion())
     }
 
-    func setMinDistance(_ distance: Float) {
-        (_nativeJoint as! ISpringJoint).setMinDistance(distance)
+    var minDistance: Float {
+        get {
+            _minDistance
+        }
+        set {
+            _minDistance = newValue
+            (_nativeJoint as! ISpringJoint).setMinDistance(newValue)
+        }
     }
 
-    func setMaxDistance(_ distance: Float) {
-        (_nativeJoint as! ISpringJoint).setMaxDistance(distance)
+    var maxDistance: Float {
+        get {
+            _maxDistance
+        }
+        set {
+            _maxDistance = newValue
+            (_nativeJoint as! ISpringJoint).setMaxDistance(newValue)
+        }
     }
 
-    func setTolerance(_ tolerance: Float) {
-        (_nativeJoint as! ISpringJoint).setTolerance(tolerance)
+    var tolerance: Float {
+        get {
+            _tolerance
+        }
+        set {
+            _tolerance = newValue
+            (_nativeJoint as! ISpringJoint).setTolerance(newValue)
+        }
     }
 
-    func setStiffness(_ stiffness: Float) {
-        (_nativeJoint as! ISpringJoint).setStiffness(stiffness)
+    var stiffness: Float {
+        get {
+            _stiffness
+        }
+        set {
+            _stiffness = newValue
+            (_nativeJoint as! ISpringJoint).setStiffness(newValue)
+        }
     }
 
-    func setDamping(_ damping: Float) {
-        (_nativeJoint as! ISpringJoint).setDamping(damping)
+    var damping: Float {
+        get {
+            _damping
+        }
+        set {
+            _damping = newValue
+            (_nativeJoint as! ISpringJoint).setDamping(newValue)
+        }
     }
 
-    func setDistanceJointFlag(_ flag: Int, _ value: Bool) {
-        (_nativeJoint as! ISpringJoint).setDistanceJointFlag(flag, value)
+    func setDistanceJointFlag(_ flag: SpringJointFlag, _ value: Bool) {
+        (_nativeJoint as! ISpringJoint).setDistanceJointFlag(flag.rawValue, value)
     }
 }
