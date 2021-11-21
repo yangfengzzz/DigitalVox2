e6260c9cbfb9a0f4ac02ff699def2ae8ff4394eb
yangfengzzz <yangfengzzz@hotmail.com>
2021-11-21 10:10:31 +0800

    add meshio export

--- vox.oasis/animation/offline/fbx/mesh_io.h
+++ vox.oasis/animation/offline/fbx/mesh_io.h
@@ -9,8 +9,29 @@
 #define mesh_io_h
 
 #import <Foundation/Foundation.h>
+#import <simd/simd.h>
+
+@protocol MTLDevice, MTLBuffer;
 
 @interface MeshIO : NSObject
+
+- (int)triangle_index_count;
+
+- (int)vertex_count;
+
+- (int)max_influences_count;
+
+- (bool)skinned;
+
+- (int)num_joints;
+
+- (int)highest_joint_index;
+
+- (id <MTLBuffer>)triangle_indices:(id <MTLDevice>)device;
+
+- (uint16_t)joint_remaps:(int)index;
+
+- (simd_float4x4)inverse_bind_poses:(int)index;
 
 @end
 
--- vox.oasis/animation/offline/fbx/mesh_io.mm
+++ vox.oasis/animation/offline/fbx/mesh_io.mm
@@ -7,6 +7,7 @@
 
 #import "mesh_io.h"
 #import "mesh_io+Internal.h"
+#import <Metal/Metal.h>
 
 @implementation MeshIO
 
@@ -17,5 +18,42 @@
     }
     return self;
 }
+
+- (int)triangle_index_count {
+    return _c_mesh.triangle_index_count();
+}
+
+- (int)vertex_count {
+    return _c_mesh.vertex_count();
+}
+
+- (int)max_influences_count {
+    return _c_mesh.max_influences_count();
+}
+
+- (bool)skinned {
+    return _c_mesh.skinned();
+}
+
+- (int)num_joints {
+    return _c_mesh.num_joints();
+}
+
+- (int)highest_joint_index {
+    return _c_mesh.highest_joint_index();
+}
+
+- (id <MTLBuffer>)triangle_indices:(id <MTLDevice>)device {
+    return [device newBufferWithBytes:_c_mesh.triangle_indices.data() length:_c_mesh.triangle_index_count() * sizeof(uint16_t) options:NULL];
+}
+
+- (uint16_t)joint_remaps:(int)index {
+    return _c_mesh.joint_remaps[index];
+}
+
+- (simd_float4x4)inverse_bind_poses:(int)index {
+    return _c_mesh.inverse_bind_poses[index];
+}
+
 
 @end
